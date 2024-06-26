52a597a18c0398240bd1d89c8e289b7e43213f17
Matache Razvan <rmatache@delloittece.com>
2024-06-26 13:16:12 +0300

    c

--- Cypress/support/commands.js
+++ Cypress/support/commands.js
@@ -25,8 +25,33 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('login', (username, password) => {
-    cy.get('#user-name').should('be.visible').type(username);
-    cy.get('#password').should('be.visible').type(password);
+  cy.get('#user-name').should('be.visible').type(username);
+  cy.get('#password').should('be.visible').type(password);
     cy.get('[data-test="login-button"]').should('be.visible').click();
     cy.url().should('include', '/inventory.html');
+  });
+
+  Cypress.Commands.add('badlogin', (baduser, password) => {
+    cy.get('#user-name').should('be.visible').type(baduser);
+    cy.get('#password').should('be.visible').type(password);
+    cy.get('[data-test="login-button"]').should('be.visible').click();
+    cy.wait(1000)
+    cy.get('[data-test="login-button"]').should('be.visible')
+    
+  });
+
+  Cypress.Commands.add('searchproduct', () => {
+    cy.contains('Products')
+  })
+
+  Cypress.Commands.add('addtocart', () => {
+    cy.get('.inventory_item').first()
+    cy.get('.btn_inventory').contains('Add to cart').click();
+
+  })
+
+  Cypress.Commands.add('checkcart', () => {
+    cy.get('.shopping_cart_badge').should('contain', '1').click();
+    cy.get('.cart_list').should('have.length.at.least', 1);
+    cy.get('.cart_quantity').should('have.text', '1');
   });
