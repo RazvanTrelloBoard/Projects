4b567d3b451c554e0ebd9efbcd0c117d49671090
Matache Razvan <rmatache@delloittece.com>
2024-07-05 10:45:25 +0300

    auto yaml

--- cypress.chrome.local.config.js
+++ cypress.chrome.local.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
+    supportFile: 'support/e2e.js',
+    specPattern: 'e2e/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
 
     },
