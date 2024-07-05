cafeee29f5ce7bf26a74206f3ce513e79e595546
Matache Razvan <rmatache@delloittece.com>
2024-07-05 10:53:43 +0300

    auto yaml

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
-    supportFile: 'support/e2e.js',
+    supportFile: 'cypress/support/e2e.js',
     specPattern: 'e2e/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
 
